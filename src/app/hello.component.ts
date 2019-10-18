bff73d251bb82333801fceb61b1b07ffe6dd93af
tgelibert <tgeli06grasse@free.fr>
2019-10-18 19:44:21 +0200

    1 file modified, 1 added

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -3,41 +3,7 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Test  saisie de champs</h1>
-   <form [formGroup]="testInput" (ngSubmit)="onFormSubmit()" autocomplete="off">
-
-     <div style="position:absolute; left: 0%;top: 15%;right: 0;bottom: 0;">
-     <p>Votre nom : </p>
-     <input type="text" id="nom" [formControl]="testInput.get('nom')" maxlength="10" />
-    
-     <div *ngIf="(submitted || nom.touched) && nom.errors?.required" class="help-block">
-                Veuillez saisir un nom.
-     </div>
-      
-      <div *ngIf="(submitted || nom.touched) && nom.errors?.pattern" class="help-block">
-                  La saisie comporte des caractères non autorisés
-      </div>
-     </div>
-     
-
-     <div style="position:absolute; left: 0%;top: 30%;right: 0;bottom: 0;">
-     <p>Votre prénom : </p> 
-     <input type="text" id="prenom" [formControl]="testInput.get('prenom')" maxlength="10"/>
-     <div *ngIf="(submitted || prenom.touched) && prenom.errors?.required" class="help-block">
-                Veuillez saisir un prenom.
-     </div>
-      
-      <div *ngIf="(submitted || prenom.touched) && prenom.errors?.pattern" class="help-block">
-                  La saisie comporte des caractères non autorisés
-      </div>
-    </div> 
-    
-    <div style="position:absolute; left: 60%;top: 50%;right: 0;bottom: 0;"> 
-    <button type="submit"  >submit</button>
-    </div>
-    
-    </form>
-  `,
+  templateUrl : './hello.component.html',
   styles: [`h1 { font-family: Lato;}`]
 })
 export class HelloComponent implements OnInit {
