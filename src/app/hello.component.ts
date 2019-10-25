264768b5c94a4066fe6599ea61efcb55b1dec810
tgelibert <tgeli06grasse@free.fr>
2019-10-25 09:17:16 +0200

    ajout tests buttons input

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -10,6 +10,16 @@ export class HelloComponent implements OnInit {
   @Input() name: string;
   testInput: FormGroup;
   submitted: boolean = false;
+
+   sms : string;
+   email : string;
+   courrier : string; 
+   choixEnvoi ="sms" ;
+   showcontent : string;
+   
+   radio1=true;
+
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
@@ -17,6 +27,10 @@ export class HelloComponent implements OnInit {
       nom: ['', { validators: [Validators.required , Validators.maxLength(10),  Validators.pattern("^[a-zA-Zà-ÿÀ-Ÿ '-]*$")], updateOn: 'blur'}],
       prenom: ['', { validators: [Validators.required , Validators.maxLength(10),  Validators.pattern("^[a-zA-Zà-ÿÀ-Ÿ '-]*$")], updateOn: 'blur'}]
     })
+    this.showcontent ="sms";
+    this.sms = "par SMS";
+    this.email = "par courriel";
+    this.courrier = "par courrier";
   }
 
 get nom() { return this.testInput.get('nom'); }
@@ -30,5 +44,8 @@ get prenom() { return this.testInput.get('prenom'); }
     {
     console.log(this.testInput.value)
     }
+  }
+  showContent(){
+    this.showcontent=this.choixEnvoi;
   }
 }
